0bff46260888fd3559552a78f21ef1e6e909501d
Brett <brett84c@gmail.com>
2022-02-14 17:52:30 -0500

    first main push: add todos, toggle todos, delete todos, save and load todos from localstorage, basic styling

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,13 +1,20 @@
-import { FC, useState, KeyboardEvent } from "react";
+import { FC, useState, KeyboardEvent, useEffect, ChangeEvent } from "react";
 import styles from '../styles/todo.module.css';
 
 interface Todo {
-  label:string,
+  label:string
   completed:boolean
 }
 
 const Todo:FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() =>{
+    if(localStorage.getItem('todos')){
+      setTodos(JSON.parse(localStorage.getItem('todos') as string));
+      console.log(JSON.parse(localStorage.getItem('todos') as string));
+    }
+  },[])
 
   const addTodo = (e:KeyboardEvent) => {
     if(e.key === 'Enter') {
@@ -16,34 +23,55 @@ const Todo:FC = () => {
         label:target.value,
         completed:false
       }]);
-      target.value = "";
+      localStorage.setItem('todos', JSON.stringify(todos));
+      console.log({todos});
+      target.value = '';
     }
+  }
+
+  const deleteTodo = (e:React.MouseEvent, idx:number) => {
+    const tempTodos = [...todos];
+    tempTodos.splice(idx, 1);
+    setTodos(tempTodos);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    console.log({todos});
+  }
+
+  const toggleTodoCompleted = (e:ChangeEvent, idx:number) => {
+    const tempTodos = [...todos];
+    tempTodos[idx].completed = !tempTodos[idx].completed;
+    setTodos(tempTodos);
+    localStorage.setItem('todos', JSON.stringify(tempTodos));
   }
 
   return (
     <div className={styles.todo}>
-      <h1 className={styles.title}>Todos</h1>
+      <h1 className={styles.title}>todos</h1>
       <div className={styles.main}>
         <header className={styles.header}>
           <input type="text" placeholder="What needs to be done?" className={styles.new_todo} onKeyDown={e => addTodo(e)}/>
         </header>
         <ul className={styles.todo_list}>
-          {todos.map(todo => {return(
-              <li className={styles.single_todo}>
-                <input type="checkbox" checked={todo.completed}/>
-                <label>{todo.label}</label>
-                <button></button>
+          {todos.map((todo, idx) => {return(
+              <li key={idx} className={todo.completed ? 'completed' : ''}>
+                <div className={styles.completed_toggle}>
+                  <input id={`completed_toggle_${idx}`} type="checkbox" onChange={e => toggleTodoCompleted(e, idx)} checked={todo.completed} />
+                  <label htmlFor={`completed_toggle_${idx}`}></label>
+                </div>
+                <label className={styles.todo_main_label}>{todo.label}</label>
+                <button className={styles.delete_todo} onClick={e => deleteTodo(e, idx)}></button>
               </li>
+
           )})}
         </ul>
         <footer className={styles.footer}>
           <span>{todos.filter(todo => !todo.completed).length} items left</span>
           <ul className={styles.filters}>
-            <li>All</li>  
-            <li>Active</li>
-            <li>Completed</li>  
+            <li><a>All</a></li>  
+            <li><a>Active</a></li>
+            <li><a>Completed</a></li>  
           </ul>
-          <span>Clear Completed</span>
+          <span><a>Clear Completed</a></span>
         </footer>
       </div>
     </div>
